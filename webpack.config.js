b987d260e675800e92014a0037c65abb2699f4e0
Imran <imranradziii@gmail.com>
2022-08-15 00:27:14 +0100

    changed webpack mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production' /*'development'*/,
   entry: './src/index.js',
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
