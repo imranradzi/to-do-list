c3d542db5be2935172628a571790069df71fcd29
Imran <imranradziii@gmail.com>
2022-08-18 13:24:41 +0100

    fixed overflowing content on tasks by introducing scrollbar, also some styling changes for overflowing content on notes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production' /*'development'*/,
+  mode: /*'production'*/ 'development',
   entry: './src/index.js',
-  // devtool: 'inline-source-map',
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
